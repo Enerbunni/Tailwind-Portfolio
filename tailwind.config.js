dac739d8db793edc45b9121cbac8272cb28e4cd5
Evan Haut <evanwhaut@gmail.com>
2022-09-13 13:13:16 -0700

    just work on pages please

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     content: ["./public/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
-    
-    theme: {
-      extend: {},
-    },
-    
     plugins: [
       require('tw-elements/dist/plugin')
     ],
