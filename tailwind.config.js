dae899c940f509f9e65db6b955406f52f668cbcb
Evan Haut <evanwhaut@gmail.com>
2022-09-13 18:14:44 -0700

    pleader

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  purge: {
-    enable: process.env.NODE_ENV !== 'development',
-    content: ["./public/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js", "./*.{html,js,ts}"],
+    content: ["./public/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js", './**/*.{html,js,ts}'], //prob bad
     plugins: [
       require('tw-elements/dist/plugin')
     ],
   }
-}
